ace7848719813ec5b9b77b88a4db438476ed0f90
BushraAkram11 <eagleit111@gmail.com>
2024-09-04 10:13:40 +0500

    Update irfan.py

--- irfan.py
+++ irfan.py
@@ -17,7 +17,7 @@ import re
 st.set_page_config(page_title="AI Voice Assistant", page_icon="🤖")
 
 # Centered Title and Subtitle
-st.markdown("<h1 style='text-align: center;'>__AI Voice Bot🎙️__</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>_AI Voice Bot🎙️_</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>Urdu Voice Interaction with Real-Time AI Responses</h5>", unsafe_allow_html=True)
 
 # Centered Image below Title and Subtitle with Increased Width
