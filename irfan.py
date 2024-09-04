c9d9dfc7b6ef1b4b02a1c4ff61e096e77453dd2c
BushraAkram11 <eagleit111@gmail.com>
2024-09-04 09:56:50 +0500

    Update irfan.py

--- irfan.py
+++ irfan.py
@@ -21,7 +21,7 @@ st.set_page_config(page_title="AI Voice Assistant", page_icon="🤖")
 st.markdown("<h1 style='text-align: center;'> AI Voice Bot </h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>Urdu Voice Interaction with Real-Time AI Responses</h5>", unsafe_allow_html=True)
 
-api_key = "..."  # Add your Google API key here
+api_key = "AIzaSyBXtfPk_O4kqGqyMK8iCD0KE_hfOCYAjUs"  # Add your Google API key here
 
 # Define the prompt
 prompt = ChatPromptTemplate(
